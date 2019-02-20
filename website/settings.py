4ace1bcf4fbad197a83100e48874f44c74e0de62
seevoid <martin.baty@gmail.com>
2019-02-20 11:36:26 +0100

    make it better

--- website/settings.py
+++ website/settings.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
+STATIC_ROOT = '/home/username/projects/site/assets/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
