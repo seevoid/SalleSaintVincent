75a507bd9185907832b547383dc4e45e7004b270
seevoid <martin.baty@gmail.com>
2019-02-21 17:05:07 +0100

    make it better

--- website/settings.py
+++ website/settings.py
@@ -157,7 +157,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sallechezdudu@gmail.com'
-EMAIL_HOST_PASSWORD = 'sterlanges'
+EMAIL_HOST_PASSWORD = '$duduOnFire$*103'
 
 
 ####### DEPLOY PRODUCTION ######
